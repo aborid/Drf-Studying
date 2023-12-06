2af5fda429b8ede2693d403553e1c5e61da1e730
文建翔 <2697027779@qq.com>
2023-12-06 23:03:25 +0800

    Add TT111

--- apps/order/urls.py
+++ apps/order/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path, re_path
 from .views import OrderGoodsGenericAPIView, OrderGenericAPIView,OrderDetailGenericAPIView
 # 这里是订单的路由地址配置中心
+#asdasdasd
 urlpatterns = [
     path("",OrderGenericAPIView.as_view()),
     path("goods/",OrderGoodsGenericAPIView.as_view()),
